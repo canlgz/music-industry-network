6a1c4de8306d945edd57a00bbc5c72d799aca19a
GZLIAO <guanze.liao@gmail.com>
2025-03-23 22:47:43 +0800

    Initial commit with music industry network visualization

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import MusicIndustryNetworkGraph from './src/components/music-industry-network-graph';
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen">
+      <MusicIndustryNetworkGraph />
+    </main>
+  );
+}
